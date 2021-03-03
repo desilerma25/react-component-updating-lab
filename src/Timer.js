624f96237d02640a6b4407a494b17ed14632b7e8
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-03 00:58:45 -0600

    commit tree bc8e165015cb2e24372947b0afa1bb7ef1c44249

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from 'react';
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -11,6 +11,26 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate() {
+    // changes font color randomly
+    // fires every time comp. updates 
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // prevents unnecessary updates caused by App's state changes
+  //   if (this.state.time === nextState.time) {
+  //     return false
+  //   }
+  //   return true
+  // } 
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  } 
 
   componentDidMount() {
     this.interval = setInterval(
